f396c81e7d29fb3086d68302504ef94c63b67fe2
Matthijs Wesseling <matthijswesseling@gmail.com>
2022-12-12 00:00:39 +0100

    Add building in setup

--- setup.py
+++ setup.py
@@ -1,2 +1,9 @@
-from setuptools import setup
-setup()
+from setuptools import Extension, setup
+
+pcg_cpp = Extension('PCG_CPP',
+                 sources = [
+                    'src/pypcg/pcg_wrapper.cpp',
+                ],
+                 include_dirs = ['src/pcg-cpp/include'])
+
+setup(ext_modules = [pcg_cpp])
