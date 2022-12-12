91ade0b98e15e4c5f82ca351632a46cd7a3f5dde
Matthijs Wesseling <matthijswesseling@gmail.com>
2022-12-12 17:03:09 +0100

    Fix setup script

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import Extension, setup
 
-PCGCPP = Extension('PCGCPP',
-                 sources = [
-                    'src/py_pcg/pcg_wrapper.cpp',
-                    'src/py_pcg/py_cpp_conversion.cpp',
-                ],
-                 include_dirs = [
-                    'src/pcg-cpp/include',
-                    'src/py_pcg',
-                ],
-                extra_compile_args=[
-                    '-std=c++11',
-                ])
+PCGCPP = Extension(
+    'PCGCPP',
+    sources = [
+        'src/py_pcg/normal_cdf.cpp',
+        'src/py_pcg/pcg_wrapper.cpp',
+        'src/py_pcg/py_cpp_conversion.cpp',
+    ],
+    include_dirs = [
+        'src/pcg-cpp/include',
+        'src/py_pcg',
+    ],
+    extra_compile_args=[
+        '-std=c++11',
+    ]
+)
 
 setup(ext_modules = [PCGCPP])
